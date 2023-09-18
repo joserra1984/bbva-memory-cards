9e9c37e85f159ca6632d27f188987119939568d3
Jose Ramon Navarrete Velasco <joserra.1984@gmail.com>
2023-09-18 09:58:39 +0200

    feat(ionic): initial ionic

---
+++ src/views/HomePage.vue
@@ -0,0 +1,31 @@
+<template>
+  <ion-page>
+    <ion-content :fullscreen="true">
+      <div id="container">
+        <strong>Bienvenido al juego Memory cards</strong>       
+      </div>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script setup lang="ts">
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+</script>
+
+<style scoped>
+#container {
+  text-align: center;
+  
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 50%;
+  transform: translateY(-50%);
+}
+
+#container strong {
+  font-size: 20px;
+  line-height: 26px;
+}
+
+</style>
