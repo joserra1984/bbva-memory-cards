c888f40f2b6a9a9d10a0167aeee3ac2d917576ee
Jose Ramon Navarrete Velasco <joserra.1984@gmail.com>
2023-09-18 10:12:02 +0200

    feat(pwa): vite-plugin-pwa config

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import router from './router';
 
 import { IonicVue } from '@ionic/vue';
 
+import { defineCustomElements } from '@ionic/pwa-elements/loader';
+
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/vue/css/core.css';
 
@@ -26,7 +28,9 @@ import './theme/variables.css';
 const app = createApp(App)
   .use(IonicVue)
   .use(router);
-  
+
+defineCustomElements(window);
+
 router.isReady().then(() => {
   app.mount('#app');
 });
